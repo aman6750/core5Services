1d07bd476a9a0516d1f0f779acb1bc938859b901
Aman <amanyadav6750@gmail.com>
2024-04-29 21:42:36 +0530

    created header

--- my-app/src/pages/Header.jsx
+++ my-app/src/pages/Header.jsx
@@ -1,29 +1,24 @@
 import React from 'react'
 import '../styles/Header.css'
-// import '../images'
+import { FaPhoneAlt, FaEnvelope, FaFacebook, FaTwitter, FaLinkedin, FaYoutube, FaMapMarker } from 'react-icons/fa'; // Import icons from React Icons
+
 
 function Header() {
     
   return (
-    <div id="nav">
-           <a href=""> logo</a>
-           <div id="dashboard_set">
-                <ul class="navbar_nav">
-                <li className="navbar_ul_li"><a href='' style={{ color: '#1b1b20' }}>Home</a></li>
-                 <li className="navbar_ul_li"><a href='' style={{color:'#1b1b20'}}>About Us</a></li>
-                <li className="navbar_ul_li"><a href='' style={{color:'#1b1b20'}}>Services</a></li>
-                <li className="navbar_ul_li"><a href='' style={{color:'#1b1b20'}}>Products</a></li>
-                </ul>
-                {/* <ul class="navbar_nav">
-                <li class="navbar_ul_li"><a href='' style="color:#1b1b20">Book a Demo</a></li>
-                <li class="vl"></li>
-                <li class="navbar_ul_li"><a href='' style="color:#1b1b20">Sign in</a></li> */}
-                
-                {/* <li class="navbar_ul_li" id="Zbutton" ><a href='' style="color:#fff">Go to App</a></li>            */}
-                {/* </ul> */}
-            </div>
-
-    </div>
+    <header>
+      <div className="contact-info">
+        <span><FaPhoneAlt />  9930419595</span>
+        <span><FaEnvelope /> info@core5.co.in</span>
+        <span><FaMapMarker /> Address: 123 Main St, City, Country</span>
+      </div>
+      <div className="social-links">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+        <a href="https://www.linkedin.com/company/core5-systems-and-services-private-limited/?originalSubdomain=in" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
+      </div>
+    </header>
   )
 }
 
